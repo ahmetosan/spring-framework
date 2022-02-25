3dda97f909303f3c6264668d04826dba4144649d
ahmetosan <ahmetpcf@gmail.com>
2022-02-24 19:41:13 -0500

    SpringBoot First Package created

--- Spring_01_Core_IoC/src/main/java/com/cydeo/bean_annotation/ConfigAny.java
+++ Spring_01_Core_IoC/src/main/java/com/cydeo/bean_annotation/ConfigAny.java
@@ -1,7 +1,9 @@
 package com.cydeo.bean_annotation;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ConfigAny {
 
 
