50a8fdaed68984519b3939de2c5126e92e87929c
ahmetosan <ahmetpcf@gmail.com>
2022-03-01 19:27:42 -0500

    Spring MVC Controller

---
+++ Spring_07_Spring_MVC_Controller/src/main/java/com/cydeo/spring_07_spring_mvc_controller/ControllerApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring_07_spring_mvc_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication.class, args);
+    }
+
+}
---
+++ Spring_07_Spring_MVC_Controller/src/test/java/com/cydeo/spring_07_spring_mvc_controller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring_07_spring_mvc_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
