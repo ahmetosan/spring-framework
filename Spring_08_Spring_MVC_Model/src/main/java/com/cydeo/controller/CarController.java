be04da0465ca2cab8f089a1738de29373dde5884
ahmetosan <ahmetpcf@gmail.com>
2022-03-03 21:26:13 -0500

    RequestParam & PathVariable 2

--- Spring_08_Spring_MVC_Model/src/main/java/com/cydeo/controller/CarController.java
+++ Spring_08_Spring_MVC_Model/src/main/java/com/cydeo/controller/CarController.java
@@ -30,11 +30,10 @@ public class CarController {
     }
 
     @RequestMapping("/info3/{make}")
-    public String carInfo3(@PathVariable String make, Model model){
+    public String carInfo3(@PathVariable String make, @PathVariable Integer year, Model model){
        model.addAttribute("make",make);
+        model.addAttribute("year",year);
         return "car/car-info";
     }
-
-
 
 }
