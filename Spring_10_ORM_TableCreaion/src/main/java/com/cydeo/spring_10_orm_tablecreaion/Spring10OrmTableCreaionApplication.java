555e0067939d2bc91f4935c352b23032b51e0e84
ahmetosan <ahmetpcf@gmail.com>
2022-04-12 19:30:13 -0400

    ORM 01

---
+++ Spring_10_ORM_TableCreaion/src/main/java/com/cydeo/spring_10_orm_tablecreaion/Spring10OrmTableCreaionApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring_10_orm_tablecreaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10OrmTableCreaionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring10OrmTableCreaionApplication.class, args);
+    }
+
+}
---
+++ Spring_10_ORM_TableCreaion/src/test/java/com/cydeo/spring_10_orm_tablecreaion/Spring10OrmTableCreaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring_10_orm_tablecreaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10OrmTableCreaionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
