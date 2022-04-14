119405cc35a5fcbaebdacb6e7077a42902285dfd
ahmetosan <ahmetpcf@gmail.com>
2022-04-14 19:36:39 -0400

    ORM 04 - annotation and tables

--- Spring_10_ORM_TableCreaion/src/main/java/com/cydeo/entity/Student.java
+++ Spring_10_ORM_TableCreaion/src/main/java/com/cydeo/entity/Student.java
@@ -1,17 +1,24 @@
 package com.cydeo.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class Student {
 
         @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
 
+        @Column(name = "studentFirstName")
         private String firstName;
+        @Column(name = "studentLastName")
         private String lastName;
         private String email;
 
+        @Transient  // bu anotaion kullaninca table ekleme yapmiyor
+        private String city;
+
 }
+
