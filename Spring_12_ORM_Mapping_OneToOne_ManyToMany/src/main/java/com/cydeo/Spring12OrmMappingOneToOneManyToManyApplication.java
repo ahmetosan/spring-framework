268a49a8cd3d49c77f316d0be36f6bba0c004f4f
ahmetosan <ahmetpcf@gmail.com>
2022-04-21 18:42:58 -0400

    ORM 09 - Mapping and ManyToMany

---
+++ Spring_12_ORM_Mapping_OneToOne_ManyToMany/src/main/java/com/cydeo/Spring12OrmMappingOneToOneManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring12OrmMappingOneToOneManyToManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring12OrmMappingOneToOneManyToManyApplication.class, args);
+    }
+
+}
---
+++ Spring_12_ORM_Mapping_OneToOne_ManyToMany/src/test/java/com/cydeo/spring_12_orm_mapping_onetoone_manytomany/Spring12OrmMappingOneToOneManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring_12_orm_mapping_onetoone_manytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring12OrmMappingOneToOneManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
