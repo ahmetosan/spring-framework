4bfd5bad72bb3e75fd79d8cca4ac2c05b3a728af
ahmetosan <ahmetpcf@gmail.com>
2022-04-23 13:52:56 -0400

    ORM 13 - Lab1(second hrs)V3

--- Spring_13_ORM_CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
+++ Spring_13_ORM_CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
@@ -6,22 +6,16 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
-import javax.xml.stream.Location;
 
 
 @Entity
 @Data
 @NoArgsConstructor
-
 public class Cinema extends BaseEntity{
 
-
     private String name;
-    private String sponsorName;
-
+    private String sponsoredName;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Location location;
-
-
 }
