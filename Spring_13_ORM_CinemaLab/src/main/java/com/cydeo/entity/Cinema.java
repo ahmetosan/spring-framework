45b3df2de2fd90319607bed1f6bfdd6b09dbd5ff
ahmetosan <ahmetpcf@gmail.com>
2022-04-23 11:33:16 -0400

    ORM 13 - Lab1(second hrs)V2

--- Spring_13_ORM_CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
+++ Spring_13_ORM_CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
@@ -1,4 +1,27 @@
 package com.cydeo.entity;
 
-public class Cinema {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.xml.stream.Location;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class Cinema extends BaseEntity{
+
+
+    private String name;
+    private String sponsorName;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Location location;
+
+
 }
--- Spring_13_ORM_CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
+++ Spring_13_ORM_CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
@@ -3,26 +3,27 @@ package com.cydeo.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class MovieCinema extends BaseEntity{
+public class Ticket extends BaseEntity {
+
+    private Integer seatNumber;
+    private Integer rowNumber;
 
 
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime dateTime;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Movie movie;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Cinema cinema;
+    private MovieCinema movieCinema;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User userAccount;
 
 
 }
