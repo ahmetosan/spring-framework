d1d13f6b4c968f56c41cbbf93e41c5679436874b
ahmetosan <ahmetpcf@gmail.com>
2022-06-02 18:56:06 -0400

    Api Day 1

--- Spring_13_ORM_CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ Spring_13_ORM_CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -4,6 +4,7 @@ import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
